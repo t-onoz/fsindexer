33976ff6a79104ae22c623490bee166ac0c302b5
t-onoz <18749921+t-onoz@users.noreply.github.com>
2026-07-26 20:27:32 +0900

    デバッグログの有効化/無効化機能をインデクサーに追加

--- src/fsindexer/__init__.py
+++ src/fsindexer/__init__.py
@@ -1,3 +1,3 @@
-from ._fsindexer import FileIndexer, MarkedNode
+from ._fsindexer import FileIndexer, MarkedNode, disable_debug_log, enable_debug_log
 
-__all__ = ["FileIndexer", "MarkedNode"]
+__all__ = ["FileIndexer", "MarkedNode", "enable_debug_log", "disable_debug_log"]
